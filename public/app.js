5d13cae1d86a14949ca69254f5aece4f6bed294a
Star Lee <zabra15@yahoo.com>
2017-12-19 18:25:40 -0500

    added Save Article buttons

--- public/app.js
+++ public/app.js
@@ -1,11 +1,15 @@
 $.getJSON("/articles", function(data) {
     for (var i = 0; i < data.length; i++) {
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+        $("#articles").append("<div class='singleArticle' data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link +
+            "<p><a id='save' class='btn btn-secondary btn-sm\' href='./saved.html' role='button'>Save Article</a></p>" +
+            "</div>");
     }
 });
 
 
-// // Whenever someone clicks the Save Article button
+// // // Whenever someone clicks the Save Article button ====
+// ======== Unable to get this to work without it killing the scrape ===========
+
 // $("#save").on("click", function() {
 //
 //     // Save the id
@@ -26,30 +30,31 @@ $.getJSON("/articles", function(data) {
 // });
 
 // When you click the savenote button
-$(document).on("click", "#savenote", function() {
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
+//=====Can't use/test this until I get the Save Article button working ===========
 
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-        method: "POST",
-        url: "/articles/" + thisId,
-        data: {
-            // Value taken from title input
-            title: $("#titleinput").val(),
-            // Value taken from note textarea
-            body: $("#bodyinput").val()
-        }
-    })
-    // With that done
-        .done(function(data) {
-            // Log the response
-            console.log(data);
-            // Empty the notes section
-            $("#notes").empty();
-        });
+// $(document).on("click", "#savenote", function() {
+//     // Grab the id associated with the article from the submit button
+//     var thisId = $(this).attr("data-id");
+//
+//     // Run a POST request to change the note, using what's entered in the inputs
+//     $.ajax({
+//         method: "POST",
+//         url: "/articles/" + thisId,
+//         data: {
+//             // Value taken from title input
+//             title: $("#titleinput").val(),
+//             // Value taken from note textarea
+//             body: $("#bodyinput").val()
+//         }
+//     })
+//     // With that done
+//         .done(function(data) {
+//             // Log the response
+//             console.log(data);
+//             // Empty the notes section
+//             $("#notes").empty();
+//         });
 
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
-});
+//     $("#titleinput").val("");
+//     $("#bodyinput").val("");
+// });
