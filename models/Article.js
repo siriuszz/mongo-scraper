c694531c718ef58079cf2b211ccaa0899b81f300
Star Lee <zabra15@yahoo.com>
2017-12-19 14:19:14 -0500

    scrape working

--- models/Article.js
+++ models/Article.js
@@ -8,6 +8,14 @@ var ArticleSchema = new Schema({
         type: String,
         required: true
     },
+    // byline: {
+    //     type: String,
+    //     required: true
+    // },
+    // summary: {
+    //     type: String,
+    //     required: true
+    // },
     link: {
         type: String,
         required: true
