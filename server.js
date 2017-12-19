ce9c235da3c49f63ead563bb480b1552b335df1d
Star Lee <zabra15@yahoo.com>
2017-12-18 20:58:11 -0500

    mongolab URL

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 // For scraping the site
 var cheerio = require("cheerio");
-var request = require("request");
+var axios = require("axios");
 
 // // For keeping notes
 var express = require("express");
@@ -11,7 +11,7 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 
 
-// var db = require("./models");
+var db = require("./models");
 
 var PORT = 8080;
 
@@ -23,30 +23,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 
-//B config with Mongoose
-var databaseUri = 'mongodb://localhost/imdbarticles';
-
-if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
-} else {
-    mongoose.connect(databaseUri);
-}
-
-var db = mongoose.connection;
-
-db.on("error", function (err) {
-    console.log("Mongoose Error: ", err);
-});
-
-db.once("open", function () {
-    console.log("Mongoose connection successful.");
-});
-
-
-// mongoose.Promise = Promise;
-// mongoose.connect("mongodb://localhost/gtbc-mongo-scraper", {
-//     useMongoClient: true
+// Config with Mongoose
+// var databaseUri = 'mongodb://localhost/imdbarticles';
+//
+// if (process.env.MONGODB_URI) {
+//     mongoose.connect(process.env.MONGODB_URI);
+// } else {
+//     mongoose.connect(databaseUri);
+// }
+//
+// var db = mongoose.connection;
+//
+// db.on("error", function (err) {
+//     console.log("Mongoose Error: ", err);
 // });
+//
+// db.once("open", function () {
+//     console.log("Mongoose connection successful.");
+// });
+
+
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://heroku_vmkrqd84:vk1am47r0ktnikeffcdgu1qe8@ds141786.mlab.com:41786/heroku_vmkrqd84", {
+    useMongoClient: true
+});
 
 
 // Routes ===================
@@ -85,18 +85,18 @@ app.get("/scrape", function(req, res) {
     });
 });
 
-// // Route for getting all Articles from the db
-// app.get("/articles", function(req, res) {
-//     db.Article
-//         .find({})
-//         .then(function(dbArticle) {
-//             // Send articles back to the client
-//             res.json(dbArticle);
-//         })
-//         .catch(function(err) {
-//             res.json(err);
-//         });
-// });
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+    db.Article
+        .find({})
+        .then(function(dbArticle) {
+            // Send articles back to the client
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
@@ -115,15 +115,12 @@ app.get("/articles/:id", function(req, res) {
         });
 });
 
-// Route for saving/updating an Article's associated Note
+// // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
     // Create a new note and pass the req.body to the entry
     db.Note
         .create(req.body)
         .then(function(dbNote) {
-            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
             return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
         })
         .then(function(dbArticle) {
@@ -136,7 +133,7 @@ app.post("/articles/:id", function(req, res) {
         });
 });
 //
-//
+
 // // Listen on port 8080
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + ".");
